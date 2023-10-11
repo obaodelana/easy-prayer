08ae72f653538bcd78374a3d27222aba569aa1d4
Obaloluwa Odelana <35340328+obaodelana@users.noreply.github.com>
2023-10-11 17:28:34 -0600

    Also remove carriage returns

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-const bibleBooks = [
-	"Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy",
-	"Joshua", "Judges", "Ruth",
-	"1 Samuel", "2 Samuel",
-	"1 Kings", "2 Kings",
-	"1 Chronicles", "2 Chronicles",
-	"Ezra", "Nehemiah",
-	"Esther",
-	"Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Solomon",
-	"Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel",
-	"Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum",
-	"Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi",
-	"Matthew", "Mark", "Luke", "John",
-	"Acts",
-	"Romans",
-	"1 Corinthians", "2 Corinthians",
-	"Galatians", "Ephesians", "Philippians", "Colossians",
-	"1 Thessalonians", "2 Thessalonians",
-	"1 Timothy", "2 Timothy",
-	"Titus", "Philemon",
-	"Hebrews",
-	"James",
-	"1 Peter", "2 Peter",
-	"1 John", "2 John", "3 John",
-	"Jude",
-	"Revelation"
-];
-
 class Prayer {
 	number = 1;
 	text = "";
@@ -60,7 +32,7 @@ class Parser {
 	constructor(text) {
 		const separators = /(?:\s*(\d\s*\w+[.]*)(\s*)(\d+))|(\d+)|(\s+)/;
 		this.words = text.split(separators).filter((w) => {
-			return w != undefined && !w.includes('\n') && w.length > 0
+			return w != undefined && !w.includes('\n') && !w.includes('\r') && w.length > 0
 		});
 		// console.log(this.words);
 		// return;
